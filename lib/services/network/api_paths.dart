5553603af8e74940bdcb410a7185e02bd41d4ac0
ashiksaini <ashik.saini@aurigait.com>
2023-12-05 20:31:12 +0530

    cc

--- lib/services/network/api_paths.dart
+++ lib/services/network/api_paths.dart
@@ -3,4 +3,5 @@ class ApiPaths {
   static const String resetPassword = 'custom/v1/forget_password';
   static const String registerUser = 'moomalapi/v1/moomalcreateuser';
   static const String products = 'wc/v3/products';
+  static const String search = 'wp/v2/search/';
 }
