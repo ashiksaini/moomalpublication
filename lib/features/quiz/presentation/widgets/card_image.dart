ada590f519e381a833cfcca9d599a533f1ca2576
ashiksaini <ashik.saini@aurigait.com>
2024-01-23 18:17:31 +0530

    merge

--- lib/features/quiz/presentation/widgets/card_image.dart
+++ lib/features/quiz/presentation/widgets/card_image.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
 import 'package:moomalpublication/core/theme/dimen.dart';
 
 class CardImage extends StatelessWidget {
