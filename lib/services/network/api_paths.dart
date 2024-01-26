ac1350a1df3f30819430f8076d996e8124f141a4
ashiksaini <ashik.saini@aurigait.com>
2024-01-26 15:44:38 +0530

    search api added

--- lib/services/network/api_paths.dart
+++ lib/services/network/api_paths.dart
@@ -4,6 +4,7 @@ class ApiPaths {
   static const String resetPassword = 'custom/v1/forget_password';
   static const String registerUser = 'moomalapi/v1/moomalcreateuser';
   static const String products = 'moomal/v2/products';
+  static const String searchProducts = 'wp/v2/search/';
   static const String productDetail = 'moomal/v2/product/';
   static const String productComments = '/moomal/v2/product-comments/';
   static const String productReviews = 'moomal/v2/product-reviews/';
