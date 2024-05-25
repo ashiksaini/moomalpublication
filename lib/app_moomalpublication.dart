b5b113b066bdb2e5a423094074299df44627ddb3
Vishakha Chauhan <vishakha.chauhan@aurigait.com>
2024-05-25 22:40:18 +0530

    changed intial route

--- lib/app_moomalpublication.dart
+++ lib/app_moomalpublication.dart
@@ -31,7 +31,7 @@ class _MoomalPublicationState extends State<MoomalPublication> {
         translationsKeys: AppTranslation.translationKeys,
         title: "moomalpublication".tr,
         getPages: AppPages.pages,
-        initialRoute: NameRoutes.webView,
+        initialRoute: NameRoutes.splashScreen,
         theme: ThemeData(
           useMaterial3: true,
         ),
