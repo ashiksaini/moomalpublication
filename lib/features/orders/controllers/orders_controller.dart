50e73276fd2ac87f3df8efa9517a2c0bc17ccd4d
Vishakha Chauhan <vishakha.chauhan@aurigait.com>
2024-06-02 15:12:11 +0530

    order work

--- lib/features/orders/controllers/orders_controller.dart
+++ lib/features/orders/controllers/orders_controller.dart
@@ -1,5 +1,6 @@
 import 'package:get/get_rx/src/rx_types/rx_types.dart';
 import 'package:moomalpublication/core/base/base_controller.dart';
+import 'package:moomalpublication/features/orders/data/constants/enums.dart';
 import 'package:moomalpublication/features/orders/data/constants/type_alias.dart';
 import 'package:moomalpublication/features/orders/data/models/order_response_model.dart';
 import 'package:moomalpublication/features/orders/data/services/get_orders_services.dart';
@@ -8,7 +9,8 @@ import 'package:moomalpublication/services/network/api_reponse.dart';
 class OrderController extends BaseController {
   final Rx<OrderResponse> orderResponse = Rx(ApiResponse());
   final RxBool showBrowseProduct = true.obs;
-  final RxList<OrderResponseModel> ordersList = RxList<OrderResponseModel>([]);
+  RxList<OrderResponseModel> ordersList = RxList<OrderResponseModel>([]);
+
   final Rx<OrderResponseModel> singleProduct = Rx(OrderResponseModel());
 
   @override
@@ -26,5 +28,32 @@ class OrderController extends BaseController {
     } else {
       showBrowseProduct.value = false;
     }
+  }
+
+  Rx<OrdersMainTabType> selectedMainTestType = Rx(OrdersMainTabType.ebook);
+
+  RxList<OrderResponseModel> ebookPurchase = RxList();
+  RxList<OrderResponseModel> bookPurchase = RxList();
+  RxList<OrderResponseModel> testSeries = RxList();
+
+  void onMainTabsClick(OrdersMainTabType orderMainTabType) {
+    selectedMainTestType.value = orderMainTabType;
+
+    switch (orderMainTabType) {
+      case OrdersMainTabType.ebook:
+        ebookPurchase = _getList();
+        ordersList = ebookPurchase;
+      case OrdersMainTabType.book:
+        bookPurchase = _getList();
+        ordersList = bookPurchase;
+      case OrdersMainTabType.test:
+        testSeries = _getList();
+        ordersList = testSeries;
+    }
+    ordersList.refresh();
+  }
+
+  RxList<OrderResponseModel> _getList() {
+    return ordersList;
   }
 }
---
+++ lib/features/orders/presentation/widgets/orders_main_tab_item.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/widgets.dart';
+import 'package:moomalpublication/core/components/atoms/custom_text.dart';
+import 'package:moomalpublication/core/theme/box_shadows.dart';
+import 'package:moomalpublication/core/theme/colors.dart';
+import 'package:moomalpublication/core/theme/custom_text_style.dart';
+import 'package:moomalpublication/core/theme/dimen.dart';
+import 'package:moomalpublication/features/orders/data/constants/enums.dart';
+
+class OrdersMainTabItem extends StatelessWidget {
+  final String title;
+  final OrdersMainTabType orderMainTabType;
+  final Function onClick;
+  final bool isSelected;
+
+  const OrdersMainTabItem({
+    super.key,
+    required this.title,
+    required this.orderMainTabType,
+    required this.onClick,
+    this.isSelected = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => onClick(orderMainTabType),
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 30.h, vertical: 12.v),
+        decoration: BoxDecoration(
+            color: isSelected ? AppColors.orange : AppColors.white,
+            borderRadius: BorderRadius.circular(10.r),
+            border: Border.all(
+                color: isSelected ? AppColors.white : AppColors.grey),
+            boxShadow: isSelected ? [primaryBoxShadow()] : null),
+        child: Center(
+          child: CustomText(
+            text: title,
+            textStyle: CustomTextStyle.textStyle20Bold(
+              context,
+              color: isSelected ? AppColors.white : AppColors.grey,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
